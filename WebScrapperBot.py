13c26f890b0bda2845b69fd643128ec9376e9cb8
Bartenderr <b.valentino80@gmail.com>
2022-06-09 21:28:58 +0100

    This is the first version of My WebScrapperBot

---
+++ WebScrapperBot.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+try:
+    source = requests.get('https://www.cbinsights.com/research/well-funded-startups-us-map/')  # Table ---'https://darkgalaxies.mypinata.cloud/ipfs/QmUcyiWazZiAmLTKBds3niZMnA8pxSHA3rAtbRu8pL6jCY'
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+    name = soup.find('tbody').find_all('tr')
+
+    print(source.status_code)
+
+    with open('startuporg.csv', 'w', newline='') as starter:
+        fieldnames = ['Location', 'Name', 'Total Equity']
+        writer = csv.DictWriter(starter, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for startup in name:
+            company_loc = startup.find_all('td')[0].text
+            company_name = startup.find_all('td')[1].text
+            equity = startup.find_all('td')[2].text
+
+            writer.writerow({'Location': company_loc, 'Name': company_name, 'Total Equity': equity})
+            print(company_loc, company_name, equity)
+except Exception as e:
+    print(e)
